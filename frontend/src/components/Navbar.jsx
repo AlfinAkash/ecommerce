042fd8dda9642e97d9b23b4b2edc5bac729d1c63
AlfinAkash <alfinakash2001@gmail.com>
2025-06-01 14:30:50 +0530

    Upadete

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -13,7 +13,7 @@ const Navbar = () => {
 			<div className='container mx-auto px-4 py-3'>
 				<div className='flex flex-wrap justify-between items-center'>
 					<Link to='/' className='text-2xl font-bold text-emerald-400 items-center space-x-2 flex'>
-						E-Commerce
+						AlfinAkash E-Commerce
 					</Link>
 
 					<nav className='flex flex-wrap items-center gap-4'>
